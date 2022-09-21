8eb2e68d30a45772499547867ea3050655e830f0
Eduen Sarceño <s.aeduen@gmail.com>
2022-09-20 21:24:18 -0600

    Crear una navbar simple

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,16 @@
 import '../app.css';
 </script>
 
+<div class="flex flex-col min-h-screen">
+<header class="flex-none navbar bg-base-300 text-base-content">
+<div class="flex-1">
+  <a class="btn btn-ghost text-xl">Your logo</a>
+</div>
+<div class="flex-none">
+  <a class="btn btn-ghost text-xl">Menu</a>
+</div>
+</header>
+<div class="flex-1 flex flex-col">
 <slot/>
+</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<div class="hero min-h-screen bg-base-200">
+<div class="hero flex-1">
 <div class="hero-content text-center">
   <div class="max-w-md">
-  <h1 class="text-6xl font-bold bg-base-200">Welcome to SvelteKit</h1>
-  <p class="py-6">Visit <a href="https://kit.svelte.dev" class="btn btn-sm">kit.svelte.dev</a> to read the documentation</p>
+  <h1 class="text-6xl font-bold">Welcome to SvelteKit</h1>
+  <p class="py-6">Visit <a href="https://kit.svelte.dev" class="btn btn-sm btn-primary">kit.svelte.dev</a> to read the documentation</p>
   </div>
 </div>
 </div>
