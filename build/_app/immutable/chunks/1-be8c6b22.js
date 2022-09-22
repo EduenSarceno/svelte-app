3cc42cc42430da275f35cab16e2b5eca23e948c2
Eduen Sarceño <s.aeduen@gmail.com>
2022-09-22 17:08:22 -0600

    mover archivos a /build

---
+++ build/_app/immutable/chunks/1-be8c6b22.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-5a9fb8d1.js";export{t as component};
---
+++ build/_app/immutable/chunks/2-95dea52c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-28badf0a.js";export{t as component};
