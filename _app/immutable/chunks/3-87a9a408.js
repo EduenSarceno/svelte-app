9cb9691478c43e43f3fd892569fadde40fdfbe40
Eduen Sarceño <s.aeduen@gmail.com>
2022-09-21 21:38:59 -0600

    v0.1-next

---
+++ _app/immutable/chunks/3-87a9a408.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/about-us/_page.svelte-676b97ee.js";export{t as component};
---
+++ _app/immutable/chunks/5-bbb649a7.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/posts/_page.svelte-669f8349.js";export{t as component};
